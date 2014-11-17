2a42e7d87e89687c371208250e85e3c5cedb25bc
Fred Bradley <hello@fredbradley.co.uk>
2014-11-17 10:11:40 +0000

    First Commit

---
+++ fred-bradley-google-map.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Freds Google Map Plugin.
+ * Plugin URI: http://fredbradley.uk
+ * Description: Written bespokely for EmilyFruitCrisps.co.uk
+ * Version: 1.0.0
+ * Author: Fred Bradley
+ * Author URI: http://www.fredbradley.uk
+ * License: GPL2
+ */
